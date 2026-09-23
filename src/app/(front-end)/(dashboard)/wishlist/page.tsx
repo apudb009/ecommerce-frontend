da6a68d91406f392695914552818da39c025b1c8
Apurba Bhowmik <apurba@bluemint.dev>
2026-09-23 11:38:18 +0530

    Fixed cache issues

--- src/app/(front-end)/(dashboard)/wishlist/page.tsx
+++ src/app/(front-end)/(dashboard)/wishlist/page.tsx
@@ -4,6 +4,5 @@ import WishlistClient from '@/components/shop/wishlist/WishlistClient';
 
 export default async function WishlistPage() {
   const wishlist = await serverFetch<Wishlist>('/wishlist');
-  console.info(wishlist);
   return <WishlistClient wishlist={wishlist} />;
 }
